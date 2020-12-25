6825241deb4b06567e1f36c88206831f6014c1f1
Pulkit <42323214+zinader@users.noreply.github.com>
2020-12-25 16:19:10 +0530

    scraper-added

---
+++ instep_scraper.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import pandas as pd
+import unicodedata
+import re
+import numpy as np
+import csv
+
+
+URL = "https://www.infosys.com/instep/internship/projects.html"
+
+r = requests.get(URL) 
+soup = BeautifulSoup(r.content, 'html.parser') 
+   
+# Dict to be converted to df
+#print(soup)
+majors = {}
+links = soup.findAll('td',title="Academic Concentration")
+links2 = soup.findAll('td',title="Project Area")
+links3 = soup.findAll('td',title="Duration (in weeks)")
+links4 = soup.findAll('td',title="Description")
+
+#print(links)
+rows = []
+i = 0
+
+with open('instep.csv', 'w', newline='') as file:#Find all outbound links on succsesor pages and explore each one 
+    writer = csv.writer(file)
+    writer.writerow(["Academic Concentration","Project Area", "Duration","Description"])
+
+    while(i<len(links)):
+        writer.writerow([links[i].text,links2[i].text,links3[i].text,links4[i].text,])
+        i = i+1
